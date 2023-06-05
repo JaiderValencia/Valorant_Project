8bd0a70ea2abae398f6944aed8eeb4c0448ce1ef
JaiderValencia <janvame05@gmail.com>
2023-06-05 17:35:40 -0500

    hago el navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,15 @@
+import { Routes, Route } from 'react-router-dom'
 import './App.css'
+import NavBar from './NavBar/NavBar'
 
 function App() {
   return (
     <>
-      
+      <NavBar />
+
+      <Routes>
+        <Route path='/agentes'>
+      </Routes>
     </>
   )
 }
---
+++ src/NavBar/NavBar.jsx
@@ -0,0 +1,32 @@
+import './NavBar.css'
+
+function NavBar() {
+    return (
+        <nav className="navbar navbar-expand-lg bg-light">
+            <div className="container-fluid">
+                <a className="navbar-brand" href="#">
+                    <svg xmlns="http://www.w3.org/2000/svg" className="icon icon-tabler icon-tabler-brand-valorant" width={24} height={24} viewBox="0 0 24 24" strokeWidth={2} stroke="currentColor" fill="none" strokeLinecap="round" strokeLinejoin="round">
+                        <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
+                        <path d="M14.5 14h4.5l2 -2v-6z"></path>
+                        <path d="M9 19h5l-11 -13v6z"></path>
+                    </svg>
+                </a>
+                <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                    <span className="navbar-toggler-icon"></span>
+                </button>
+                <div className="collapse navbar-collapse" id="navbarSupportedContent">
+                    <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+                        <li className="nav-item">
+                            <a className="nav-link" href="#">Inicio</a>
+                        </li>
+                        <li className="nav-item">
+                            <a className="nav-link" href="#">Agentes</a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+    )
+}
+
+export default NavBar
