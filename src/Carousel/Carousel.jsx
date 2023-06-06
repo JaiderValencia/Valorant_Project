61f03ae76af22bec66cf4bea715feffcd0f9885b
JaiderValencia <janvame05@gmail.com>
2023-06-06 14:48:48 -0500

    creo el carousel

--- src/Carousel/Carousel.jsx
+++ src/Carousel/Carousel.jsx
@@ -1,17 +1,31 @@
+import { useEffect, useState } from 'react'
 import Card from '../Card/Card'
 import './Carousel.css'
 
 function Carousel() {
+    const [agents, setAgents] = useState([])
+
+    const [content, setContent] = useState([])
+
+    useEffect(() => {
+        fetch("https://valorant-api.com/v1/agents?language=es-ES")
+            .then(res => res.json())
+            .then(data => setAgents(data.data))
+            .catch((e) => setAgents([]))
+    }, [])
+
+    useEffect(() => {
+        setContent(agents.map((agent, i) => (
+            <div key={i} className={`carousel-item ${i == 0 ? "active" : ""} `}>
+                <Card name={agent.displayName} image={agent.displayIcon} />
+            </div>
+        )))
+    }, [agents])
+
     return (
         <div id="carouselExampleControls" className="carousel slide" data-bs-ride="carousel">
             <div className="carousel-inner">
-                <div className="carousel-item active">
-                    <div className="cards-wrapper">
-                        <Card />
-                        <Card />
-                        <Card />
-                    </div>
-                </div>                
+                {content}
             </div>
             <button className="carousel-control-prev" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="prev">
                 <span className="carousel-control-prev-icon" aria-hidden="true"></span>
